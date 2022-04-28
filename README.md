3f46bdabe4090274784ec7085ca601941ebf2081
beshiralkhadra <beshiralkhadra@gmail.com>
2022-04-28 14:07:09 +0300

    upload all files related to red hat

---
+++ README.md
@@ -0,0 +1,66 @@
+Red Hat Decision Manager 7 Install Demo
+=======================================
+Project to automate the installation of this product without preconfiguration beyond a single admin user.
+
+You can install this project on your own machine or on an OpenShift Container Platform.
+
+
+Installing on local machine
+---------------------------
+1. [Download and unzip](https://github.com/jbossdemocentral/rhdm7-install-demo/archive/master.zip) 
+   or [clone this repo](https://github.com/jbossdemocentral/rhdm7-install-demo.git).
+
+2. Add products to installs directory, see installs/README for details and links.
+
+3. Run 'init.sh' or 'init.bat' file. 'init.bat' must be run with Administrative privileges
+
+Follow install output instructions and log in to http://localhost:8080/decision-central (u:dmAdmin / p:redhatdm1!)
+
+Kie Server access configured (u: kieserver / p:kieserver1!)
+
+Enjoy installed and configured Red Hat Decision Manager. If you need more help, see the [install video](https://youtu.be/7Vpf9i1yJXU).
+
+
+Installing on OpenShift Container Platform (CodeReady Containers)
+----------------------------------------------------------------
+Install this demo on your local machine using an OpenShift Container Platform development cluster can be 
+done using CodeReady Continers. It's fully automated and delivers a fully functioning container-native 
+development experience:
+
+[CodeReady Containers Red Hat Decision Manager Easy Install Demo](https://gitlab.com/redhatdemocentral/crc-rhdm-install-demo)
+
+
+Installing on any OpenShift Container Platform (generic)
+-------------------------------------------------------
+This demo can be installed on your own version of OpenShift Container Platform. Options and guidance can be found in the [openshift support directory README](support/openshift/README.md)
+
+
+Supporting Articles
+-------------------
+- [Video - Red Hat Decision Manager Install Demo](https://youtu.be/7Vpf9i1yJXU)
+
+- [Video - CodeReady Containers Red Hat Decision Manager Install](https://youtu.be/tn3LjSE7c6I) 
+
+- [CodeReady Containers - Getting Started with OpenShift 4.5 and Decision Management Tooling](https://dzone.com/articles/codeready-containers-getting-started-with-openshif)
+
+- [Getting Started with Red Hat Decision Manager 7](https://developers.redhat.com/blog/2018/03/19/red-hat-decision-manager-7/)
+
+
+Released versions
+-----------------
+See the tagged releases for the following versions of the product:
+
+- v1.9 - Red Hat Decision Manager 7.11.0 GA
+- v1.8 - Red Hat Decision Manager 7.10.0 GA
+- v1.7 - Red Hat Decision Manager 7.9.0 GA
+- v1.6 - Red Hat Decision Manager 7.8.0 GA
+- v1.5 - Red Hat Decision Manager 7.7.0 GA
+- v1.4 - Red Hat Decision Manager 7.5.0.GA
+- v1.3 - Red Hat Decision Manager 7.3.0.GA
+- v1.2 - Red Hat Decision Manager 7.2.0.GA
+- v1.1 - Red Hat Decision Manager 7.1.0.GA
+- v1.0 - Red Hat Decision Manager 7.0.0.GA
+
+[![Red Hat Decision Manager Install Video](./docs/demo-images/install-video.png)](https://youtu.be/7Vpf9i1yJXU)
+
+![Red Hat Decision Manager 7](./docs/demo-images/rhdm7.png)
---
+++ init.sh
@@ -0,0 +1,180 @@
+#!/bin/sh 
+DEMO="Install Demo"
+AUTHORS="Red Hat"
+PROJECT="git@github.com:jbossdemocentral/rhdm7-install-demo.git"
+PRODUCT="Red Hat Decision Manager"
+JBOSS_HOME=./target/jboss-eap-7.3
+SERVER_DIR=$JBOSS_HOME/standalone/deployments/
+SERVER_CONF=$JBOSS_HOME/standalone/configuration/
+SERVER_BIN=$JBOSS_HOME/bin
+SRC_DIR=./installs
+SUPPORT_DIR=./support
+PRJ_DIR=./projects
+VERSION_EAP=7.3.0
+VERSION=7.11.0
+EAP=jboss-eap-$VERSION_EAP.zip
+RHDM=rhdm-$VERSION-decision-central-eap7-deployable.zip
+KIESERVER=rhdm-$VERSION-kie-server-ee8.zip
+
+# wipe screen.
+clear 
+
+echo
+echo "###############################################################"
+echo "##                                                           ##"   
+echo "##  Setting up the ${DEMO}                              ##"
+echo "##                                                           ##"   
+echo "##                                                           ##"   
+echo "##         ####  ##### ####     #   #  ###  #####            ##"
+echo "##         #   # #     #   #    #   # #   #   #              ##"
+echo "##         ####  ###   #   #    ##### #####   #              ##"
+echo "##         #  #  #     #   #    #   # #   #   #              ##"
+echo "##         #   # ##### ####     #   # #   #   #              ##"
+echo "##                                                           ##"   
+echo "##     ####  #####  #### #####  #### #####  ###  #   #       ##"   
+echo "##     #   # #     #       #   #       #   #   # ##  #       ##"   
+echo "##     #   # ###   #       #    ###    #   #   # # # #       ##"   
+echo "##     #   # #     #       #       #   #   #   # #  ##       ##"   
+echo "##     ####  #####  #### ##### ####  #####  ###  #   #       ##"   
+echo "##                                                           ##"   
+echo "##       #   #  ###  #   #  ###  ##### ##### ####            ##"
+echo "##       ## ## #   # ##  # #   # #     #     #   #           ##"
+echo "##       # # # ##### # # # ##### #  ## ###   ####            ##"
+echo "##       #   # #   # #  ## #   # #   # #     #  #            ##"
+echo "##       #   # #   # #   # #   # ##### ##### #   #           ##"
+echo "##                                                           ##"   
+echo "##                                                           ##"   
+echo "##  brought to you by, ${AUTHORS}                               ##"   
+echo "##                                                           ##"   
+echo "##  ${PROJECT}   ##"
+echo "##                                                           ##"   
+echo "###############################################################"
+echo
+
+# make some checks first before proceeding.	
+
+if [ -r $SRC_DIR/$EAP ] || [ -L $SRC_DIR/$EAP ]; then
+	echo "Product EAP sources are present..."
+	echo
+else
+	echo "Need to download $EAP package from https://developers.redhat.com/products/eap/download"
+	echo "and place it in the $SRC_DIR directory to proceed..."
+	echo
+	exit
+fi
+
+if [ -r $SRC_DIR/$RHDM ] || [ -L $SRC_DIR/$RHDM ]; then
+	echo "Product RHDM sources are present..."
+	echo
+else
+	echo "Need to download $RHDM from https://developers.redhat.com/products/red-hat-decision-manager/download"
+	echo "and place it in the $SRC_DIR directory to proceed..."
+	echo
+	exit
+fi
+
+if [ -r $SRC_DIR/$KIESERVER ] || [ -L $SRC_DIR/$KIESERVER ]; then
+	echo "Product RHDM Kie Server sources are present..."
+	echo
+else
+	echo "Need to download $KIESERVER from https://developers.redhat.com/products/red-hat-decision-manager/download"
+	echo "and place it in the $SRC_DIR directory to proceed..."
+	echo
+	exit
+fi
+
+# Remove the old JBoss instance, if it exists.
+if [ -x $JBOSS_HOME ]; then
+	echo "  - removing existing installation directory..."
+	echo
+	rm -rf ./target
+fi
+
+# Installation.
+echo "JBoss EAP installation running now..."
+echo
+mkdir -p ./target
+unzip -qo $SRC_DIR/$EAP -d ./target
+
+if [ $? -ne 0 ]; then
+	echo "Error occurred during JBoss EAP installation!"
+	echo
+	exit
+fi
+
+echo "Red Hat Decision Manager installation running now..."
+echo
+unzip -qo $SRC_DIR/$RHDM -d ./target 
+
+if [ $? -ne 0 ]; then
+	echo "Error occurred during $PRODUCT installation!"
+	echo
+	exit
+fi
+
+echo "Red Hat Decision Manager KIE Server installation running now..."
+echo
+unzip -qo $SRC_DIR/$KIESERVER -d $JBOSS_HOME/standalone/deployments 
+
+if [ $? -ne 0 ]; then
+	echo "Error occurred during $PRODUCT installation!"
+	echo
+	exit
+fi
+
+# Set deployment Kie Server.
+touch $JBOSS_HOME/standalone/deployments/kie-server.war.dodeploy
+
+echo "  - enabling demo accounts role setup..."
+echo
+echo "  - User 'dmAdmin' password 'redhatdm1!' setup..."
+echo
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u dmAdmin -p redhatdm1! -ro analyst,admin,manager,user,kie-server,kiemgmt,rest-all --silent 
+
+if [ $? -ne 0 ]; then
+	echo "Error occurred during user add dmAdmin!"
+	echo
+	exit
+fi
+
+echo "  - management user 'kieserver' password 'kieserver1!' setup..."
+echo
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u kieserver -p kieserver1! -ro kie-server,rest-all --silent
+
+if [ $? -ne 0 ]; then
+	echo "Error occurred during user add kieserver!"
+	echo
+	exit
+fi
+
+echo "  - setting up standalone-full.xml configuration adjustments..."
+echo
+cp $SUPPORT_DIR/standalone-full.xml $SERVER_CONF/standalone.xml
+
+echo "  - setup email notification users..."
+echo
+cp $SUPPORT_DIR/userinfo.properties $SERVER_DIR/decision-central.war/WEB-INF/classes/
+
+echo "  - making sure standalone.sh for server is executable..."
+echo
+chmod u+x $JBOSS_HOME/bin/standalone.sh
+
+echo
+echo "==========================================================================="
+echo "=                                                                         ="
+echo "=  $PRODUCT $VERSION setup complete.                         ="
+echo "=                                                                         ="
+echo "=  You can now start the $PRODUCT with:                   ="
+echo "=                                                                         ="
+echo "=                         $SERVER_BIN/standalone.sh        ="
+echo "=                                                                         ="
+echo "=  Login to Red Hat Decision Manager to start developing rules projects:  ="
+echo "=                                                                         ="
+echo "=  http://localhost:8080/decision-central                                 ="
+echo "=                                                                         ="
+echo "=  [ u:dmAdmin / p:redhatdm1! ]                                           ="
+echo "=                                                                         ="
+echo "=  See README.md for general details.                                     ="
+echo "=                                                                         ="
+echo "==========================================================================="
+echo
